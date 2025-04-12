9af57e57b1ed83f10696052f1f315fbe6a31d87d
Geert Haans <geert.haans@gmail.com>
2025-04-12 22:16:55 +0200

    feat: add necessary models for SCIM

---
+++ Conflux.Domain/Role.cs
@@ -0,0 +1,17 @@
+// This program has been developed by students from the bachelor Computer Science at Utrecht
+// University within the Software Project course.
+// 
+// © Copyright Utrecht University (Department of Information and Computing Sciences)
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Conflux.Domain;
+
+public class Role
+{
+    [Key] public required Guid Id { get; set; } = Guid.NewGuid();
+
+    public required string Name { get; init; }
+    public string? Description { get; init; }
+    public required string Urn { get; init; }
+}

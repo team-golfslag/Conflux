ceba0de0fea1f20a0ef3053a125919b6d61c1ba0
Ben Stokmans <benstokmans@hotmail.nl>
2025-04-14 16:11:25 +0200

    feat: add migration for person/people rename

---
+++ Conflux.Data/Migrations/20250414141052_RenamePersonToContributor.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Conflux.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamePersonToContributor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PersonProject");
+
+            migrationBuilder.DropTable(
+                name: "PersonRole");
+
+            migrationBuilder.DropTable(
+                name: "People");
+
+            migrationBuilder.CreateTable(
+                name: "Contributors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SRAMId = table.Column<string>(type: "text", nullable: true),
+                    ORCiD = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    GivenName = table.Column<string>(type: "text", nullable: true),
+                    FamilyName = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contributors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContributorProject",
+                columns: table => new
+                {
+                    ContributorsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProjectId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContributorProject", x => new { x.ContributorsId, x.ProjectId });
+                    table.ForeignKey(
+                        name: "FK_ContributorProject_Contributors_ContributorsId",
+                        column: x => x.ContributorsId,
+                        principalTable: "Contributors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ContributorProject_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContributorRole",
+                columns: table => new
+                {
+                    ContributorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RolesId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContributorRole", x => new { x.ContributorId, x.RolesId });
+                    table.ForeignKey(
+                        name: "FK_ContributorRole_Contributors_ContributorId",
+                        column: x => x.ContributorId,
+                        principalTable: "Contributors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ContributorRole_Roles_RolesId",
+                        column: x => x.RolesId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContributorProject_ProjectId",
+                table: "ContributorProject",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContributorRole_RolesId",
+                table: "ContributorRole",
+                column: "RolesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContributorProject");
+
+            migrationBuilder.DropTable(
+                name: "ContributorRole");
+
+            migrationBuilder.DropTable(
+                name: "Contributors");
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    FamilyName = table.Column<string>(type: "text", nullable: true),
+                    GivenName = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    ORCiD = table.Column<string>(type: "text", nullable: true),
+                    SRAMId = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonProject",
+                columns: table => new
+                {
+                    PeopleId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProjectId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonProject", x => new { x.PeopleId, x.ProjectId });
+                    table.ForeignKey(
+                        name: "FK_PersonProject_People_PeopleId",
+                        column: x => x.PeopleId,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonProject_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonRole",
+                columns: table => new
+                {
+                    PersonId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RolesId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonRole", x => new { x.PersonId, x.RolesId });
+                    table.ForeignKey(
+                        name: "FK_PersonRole_People_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonRole_Roles_RolesId",
+                        column: x => x.RolesId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonProject_ProjectId",
+                table: "PersonProject",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonRole_RolesId",
+                table: "PersonRole",
+                column: "RolesId");
+        }
+    }
+}

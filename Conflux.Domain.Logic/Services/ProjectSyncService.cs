aaa454373bed6a1a99c40248c85907c4b417bfdd
Ben Stokmans <benstokmans@hotmail.nl>
2025-04-19 08:22:31 +0200

    feat: implement project syncing

--- Conflux.Domain.Logic/Services/ProjectSyncService.cs
+++ Conflux.Domain.Logic/Services/ProjectSyncService.cs
@@ -5,6 +5,7 @@
 
 using Conflux.Data;
 using Conflux.Domain.Logic.Exceptions;
+using Conflux.Domain.Models;
 using Conflux.RepositoryConnections.SRAM;
 using Conflux.RepositoryConnections.SRAM.Models;
 using Microsoft.EntityFrameworkCore;
@@ -23,10 +24,11 @@ public class ProjectSyncService : IProjectSyncService
     }
 
     /// <summary>
-    /// TODO: Retrieve all the groups for the collaboration, and check for updates in the members.
+    /// Checks for updates to the projects or its members
     /// </summary>
     /// <param name="projectId"></param>
     /// <exception cref="ProjectNotFoundException"></exception>
+    /// <remarks>Only syncs existing roles</remarks>
     public async Task SyncProjectAsync(Guid projectId)
     {
         // Retrieve the project from the database
@@ -39,8 +41,62 @@ public class ProjectSyncService : IProjectSyncService
         SCIMGroup? apiProject = await _scimApiClient.GetSCIMGroup(project.SCIMId ?? string.Empty);
         if (apiProject == null) throw new ProjectNotFoundException(projectId);
 
+        Group projectGroup = CollaborationMapper.MapSCIMGroup("", apiProject);
+
+        // Add new members
+        var newUsers = projectGroup.Members
+            .Where(m => project.Users.All(u => m.SCIMId != u.SCIMId))
+            .Select(m => new User
+            {
+                Id = Guid.NewGuid(),
+                SRAMId = null,
+                SCIMId = m.SCIMId,
+                ORCiD = null,
+                Name = m.DisplayName,
+                Roles = [],
+                GivenName = null,
+                FamilyName = null,
+                Email = null,
+            });
+
+        project.Users.AddRange(newUsers);
+
         // Sync the project info
         project.Title = apiProject.DisplayName;
         project.Description = apiProject.SCIMGroupInfo.Description;
+
+        // Get all roles 
+        var roles = project.Users.SelectMany(p => p.Roles).DistinctBy(p => p.Id);
+        foreach (Role role in roles)
+            await SyncProjectRoleAsync(project, role);
+
+        // Update project in database
+        _confluxContext.Projects.Update(project);
+        await _confluxContext.SaveChangesAsync();
+    }
+
+    public async Task SyncProjectRoleAsync(Project project, Role role)
+    {
+        SCIMGroup? updatedScimGroup = await _scimApiClient.GetSCIMGroup(project.SCIMId ?? string.Empty);
+        if (updatedScimGroup == null) throw new ProjectNotFoundException(project.Id);
+
+        Group updatedGroup = CollaborationMapper.MapSCIMGroup(role.Urn, updatedScimGroup);
+
+        // remove role from all users
+        foreach (User user in project.Users) 
+            user.Roles.Remove(role);
+
+        Role newRole = new()
+        {
+            Id = Guid.NewGuid(),
+            ProjectId = project.Id,
+            Name = updatedGroup.DisplayName,
+            Description = updatedGroup.Description,
+            Urn = updatedGroup.Urn,
+            SCIMId = updatedGroup.SCIMId
+        };
+
+        foreach (User user in project.Users.Where(u => updatedGroup.Members.Any(m => m.SCIMId == u.SCIMId)))
+            user.Roles.Add(newRole);
     }
 }

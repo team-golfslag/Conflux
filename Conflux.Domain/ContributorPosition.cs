173abf3a78f772a11c9c68b8685ae5bae972c377
Geert Haans <geert.haans@gmail.com>
2025-04-25 08:57:09 +0200

    feat: add ContributorPosition

---
+++ Conflux.Domain/ContributorPosition.cs
@@ -0,0 +1,32 @@
+// This program has been developed by students from the bachelor Computer Science at Utrecht
+// University within the Software Project course.
+// 
+// © Copyright Utrecht University (Department of Information and Computing Sciences)
+
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Conflux.Domain;
+
+/// <summary>
+/// TODO: Move to RAiD Package.
+/// </summary>
+public enum ContributorPositionType
+{
+    PrincipalInvestigator = 307,
+    CoInvestigator = 308,
+    Partner = 309,
+    Consultant = 310,
+    Other = 311,
+}
+
+[PrimaryKey(nameof(ContributorId), nameof(Position))]
+public class ContributorPosition
+{
+    [ForeignKey(nameof(Contributor))] public required Guid ContributorId { get; init; }
+    public required ContributorPositionType Position { get; init; }
+    public string SchemaUri => "https://vocabulary.raid.org/contributor.position.schema/305";
+    public string GetUri => "https://vocabulary.raid.org/contributor.position.schema/" + (int)Position;
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+}

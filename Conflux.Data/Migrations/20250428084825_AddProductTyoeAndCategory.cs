b1e2b18e4114b11e3b4be3ef30e6331a19ca30d0
Geert Haans <geert.haans@gmail.com>
2025-04-28 11:57:15 +0200

    feat: add migrations

---
+++ Conflux.Data/Migrations/20250428084825_AddProductTyoeAndCategory.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Conflux.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductTyoeAndCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Role",
+                table: "ContributorRoles",
+                newName: "RoleType");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "ProductCategories",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductCategories", x => new { x.ProductId, x.Type });
+                    table.ForeignKey(
+                        name: "FK_ProductCategories_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductCategories");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "RoleType",
+                table: "ContributorRoles",
+                newName: "Role");
+        }
+    }
+}

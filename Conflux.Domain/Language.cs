31206ef9ed65c6c130dda24e6385dab96a2e300c
Geert Haans <geert.haans@gmail.com>
2025-05-06 14:31:05 +0200

    refactor: remove unused includes

--- Conflux.Domain/Language.cs
+++ Conflux.Domain/Language.cs
@@ -4,21 +4,18 @@
 // © Copyright Utrecht University (Department of Information and Computing Sciences)
 
 using System.ComponentModel.DataAnnotations;
-using System.Globalization;
-using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 
 namespace Conflux.Domain;
 
 [Owned]
-
 public class Language
 {
     [MaxLength(3)] public string Id { get; init; }
 
     [JsonIgnore] public string SchemaUri => "https://www.iso.org/standard/74575.html";
-    
+
     public static Language ENGLISH =>
         new()
         {

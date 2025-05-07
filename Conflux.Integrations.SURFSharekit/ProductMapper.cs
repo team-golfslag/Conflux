26160049ed6acbe903b8840e1187cdfb6f25870b
Tijmen Vis <tijmenpvis@gmail.com>
2025-05-07 16:10:50 +0200

    style: refactored for clarity

--- Conflux.Integrations.SURFSharekit/ProductMapper.cs
+++ Conflux.Integrations.SURFSharekit/ProductMapper.cs
@@ -13,23 +13,17 @@ public static class ProductMapper
     /// <summary>
     /// Given a list of SURFSharekit repo items,
     /// it maps each repo item contained within to a product.
+    /// Invalid repo items are excluded.
     /// </summary>
     /// <param name="repoItems"></param>
     /// <returns>A list of products</returns>
     public static List<Product> MultipleRepoItemsToProducts(List<SURFSharekitRepoItem> repoItems)
     {
-        List<Product> products = new List<Product>();
-        foreach (SURFSharekitRepoItem repoItem in repoItems)
-        {
-            Product? newProduct = MapProduct(repoItem);
-            if (newProduct == null){continue;}
-            products.Add(newProduct);
-        }
-        return products;
+        return repoItems.Select(MapProduct).OfType<Product>().ToList();
     }
 
     /// <summary>
-    /// Given a single SURFSHarekit repo item,
+    /// Given a single SURFSharekit repo item,
     /// it maps it to a product. If the repo item is missing crucial data, like the title, this may return null.
     /// </summary>
     /// <param name="repoItem"></param>
@@ -38,7 +32,7 @@ public static class ProductMapper
     {
         return MapProduct(repoItem);
     }
-    
+
     /// <summary>
     /// A helper function which takes a SURFSharekit repo item
     /// and converts it to a product
@@ -47,18 +41,11 @@ public static class ProductMapper
     /// <returns>A Product or null</returns>
     private static Product? MapProduct(SURFSharekitRepoItem repoItem)
     {
-        if (repoItem.Attributes == null)
-        {
+        if (repoItem.Attributes?.Title is not { } title) //product requires a title to be made
             return null;
-        }
-  
-        string? title = repoItem.Attributes.Title;
-        if (string.IsNullOrEmpty(title)) //product requires a title to be made
-        {
-            return null;
-        } 
+
         Guid newId = Guid.NewGuid();
-        Product mappedProduct = new ()
+        Product mappedProduct = new()
         {
             Title = title,
             Id = newId,
@@ -73,7 +60,7 @@ public static class ProductMapper
                 },
             ],
         };
-        
+
         return mappedProduct;
     }
 }

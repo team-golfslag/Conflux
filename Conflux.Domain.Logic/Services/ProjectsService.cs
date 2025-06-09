48bb29066a6b6317b3d3a8fd8259f80efe1084e7
Ben Stokmans <benstokmans@hotmail.nl>
2025-06-09 17:05:44 +0200

    fix: introduce end date check for title in query

--- Conflux.Domain.Logic/Services/ProjectsService.cs
+++ Conflux.Domain.Logic/Services/ProjectsService.cs
@@ -181,9 +181,9 @@ public class ProjectsService : IProjectsService
         projects = dto.OrderByType switch
         {
             OrderByType.TitleAsc => projects.OrderBy(project =>
-                project.Titles.FirstOrDefault(t => t.Type == TitleType.Primary)?.Text),
+                project.Titles.FirstOrDefault(t => t.Type == TitleType.Primary && t.EndDate == null)?.Text),
             OrderByType.TitleDesc => projects.OrderByDescending(project =>
-                project.Titles.FirstOrDefault(t => t.Type == TitleType.Primary)?.Text),
+                project.Titles.FirstOrDefault(t => t.Type == TitleType.Primary && t.EndDate == null)?.Text),
             OrderByType.StartDateAsc  => projects.OrderBy(project => project.StartDate),
             OrderByType.StartDateDesc => projects.OrderByDescending(project => project.StartDate),
             OrderByType.EndDateAsc    => projects.OrderBy(project => project.EndDate),

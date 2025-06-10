4367344a78f022844f40badf448f1e7d0441d175
Ben Stokmans <benstokmans@hotmail.nl>
2025-06-10 16:58:01 +0200

    feat: small comply

--- Conflux.Domain/Product.cs
+++ Conflux.Domain/Product.cs
@@ -44,15 +44,15 @@ public class Product
             ProductSchema.Isbn    => "https://www.isbn-international.org/",
             ProductSchema.Rrid    => "https://scicrunch.org/resolver/",
             // This is incorrect according to the spec but necessary for the current implementation with SURF RAiD.
-            ProductSchema.Archive => "https://web.archive.org/",
+            ProductSchema.Archive => "https://web.archive.org",
             _                     => throw new ArgumentOutOfRangeException(),
         };
 
     public List<ProductCategoryType> Categories { get; set; } = [];
     
     // This is incorrect according to the spec but necessary for the current implementation with SURF RAiD.
-    public static string CategorySchemaUri => "https://vocabulary.raid.org/relatedObject.category.schema/386";
+    public static string CategorySchemaUri => "https://vocabulary.raid.org/relatedObject.category.schemaUri/386";
 
     public static string GetCategoryUri(ProductCategoryType t) =>
-        $"https://vocabulary.raid.org/relatedObject.category.schema/{(int)t}";
+        $"https://vocabulary.raid.org/relatedObject.category.id/{(int)t}";
 }
